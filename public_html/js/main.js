4ad3ca7b1b9f4f5a175ad41d18f5e4e74e15cd63
peterittner <peter.ittner@gmail.com>
2016-11-13 23:51:13 +0100

    Adding 1-3 player + sound effect support

--- public_html/js/main.js
+++ public_html/js/main.js
@@ -1,7 +1,7 @@
 // ========================================================
 // Definitions
 // ========================================================
-var mainCanvas, ctx, step, steps = 0, tick = 0, period = 2, snake, snake2;
+var mainCanvas, ctx, step, steps = 0, tick = 0, period = 2, snake1, snake2, snake3, numberOfSnakes;
 
 // ========================================================
 // Initialization
@@ -9,8 +9,6 @@ var mainCanvas, ctx, step, steps = 0, tick = 0, period = 2, snake, snake2;
 function initCanvas(canvas) {
     canvas.width = window.innerWidth - 20;
     canvas.height = window.innerHeight - 20;
-//    canvas.width = 500 - 20;
-//    canvas.height = 400 - 20;
 }
 
 function drawBackground(ctx) {
@@ -31,22 +29,36 @@ function drawBackground(ctx) {
 function drawMain(ctx) {
 
     drawBackground(ctx);
+    if(numberOfSnakes >= 1) {
+        snake1.draw(ctx);
+        snake1.executeGameStep(ctx);
+    }
+    if(numberOfSnakes >= 2) {
+        snake2.draw(ctx);
+        snake2.executeGameStep(ctx);
+    }
+    
+    if(numberOfSnakes >= 3) {
+        snake3.draw(ctx);
+        snake3.executeGameStep(ctx);
+    }
 
-    
-    snake.draw(ctx);
-    snake.executeGameStep(ctx);
-        
-    snake2.draw(ctx);
-    snake2.executeGameStep(ctx);
-    
-    
     ctx.font = "20px Arial";
     ctx.fillStyle = "#fff";
     ctx.fillText("Play tron2d @ ittner.it", mainCanvas.width -250, mainCanvas.height - 40);
-    ctx.fillStyle = snake.color;
-    ctx.fillText(snake.name + " [" + snake.keyConfiguration + "] Score: " + snake.score, 10 , 25);
-    ctx.fillStyle = snake2.color;
-    ctx.fillText(snake2.name + " [" + snake2.keyConfiguration + "] Score: " + snake2.score, 10 , 50);
+    
+    if(numberOfSnakes >= 1) {
+        ctx.fillStyle = snake1.color;
+        ctx.fillText(snake1.name + " [" + snake1.keyConfiguration + "] Score: " + snake1.score, 10 , 25);
+    }
+    if(numberOfSnakes >= 2) {
+        ctx.fillStyle = snake2.color;
+        ctx.fillText(snake2.name + " [" + snake2.keyConfiguration + "] Score: " + snake2.score, 10 , 50);
+    }
+    if(numberOfSnakes >= 3) {
+        ctx.fillStyle = snake3.color;
+        ctx.fillText(snake3.name + " [" + snake3.keyConfiguration + "] Score: " + snake3.score, 10 , 75);
+    }
     console.log("Calling drawMain");
 }
 
@@ -58,60 +70,81 @@ function mainLoop(ctx) {
 }
 
 function handleKey(key) {
-    snake.handleKeyInput(key);
-    snake2.handleKeyInput(key);
+    if(numberOfSnakes >= 1) snake1.handleKeyInput(key);
+    if(numberOfSnakes >= 2) snake2.handleKeyInput(key);
+    if(numberOfSnakes >= 3) snake3.handleKeyInput(key);
 }
 
 // ========================================================
 // Main
 // ========================================================
-function main() {
-    snake =  createSnake("Detlev", "red");
-    snake2 = createSnake("Dieter", "green");
+function main(num) {
     
-    snake.makeKnownTo(snake2);
-    snake2.makeKnownTo(snake);
+    new Audio("audio/start.wav").play();
     
-    snake.keyConfiguration = "arrow";
-    snake2.keyConfiguration ="wasd";
+    numberOfSnakes = num;
+    
+    if(numberOfSnakes >= 1) snake1 = createSnake("Oberon", "#cf722d");
+    if(numberOfSnakes >= 2) snake2 = createSnake("Kermit", "#47b500");
+    if(numberOfSnakes >= 3) snake3 = createSnake("Zandra", "#ff00ff");
+    
+    // Make all snakes known to each other so that the react on collisions
+    if(numberOfSnakes >= 2) snake1.makeKnownTo(snake2);
+    if(numberOfSnakes >= 3) snake1.makeKnownTo(snake3);
+    if(numberOfSnakes >= 2) snake2.makeKnownTo(snake1);
+    if(numberOfSnakes >= 3) snake2.makeKnownTo(snake3);
+    if(numberOfSnakes >= 3) snake3.makeKnownTo(snake1);
+    if(numberOfSnakes >= 3) snake3.makeKnownTo(snake2);
+    
+    // Configure the keys of all snakes
+    if(numberOfSnakes >= 1) snake1.keyConfiguration = "arrow";
+    if(numberOfSnakes >= 2) snake2.keyConfiguration ="wasd";
+    if(numberOfSnakes >= 3) snake3.keyConfiguration ="ijkl";
     
     mainCanvas = document.getElementById("main");
-
+    mainCanvas.style.display = 'block';
+    mainCanvas.style.margin = '5px';
     //window.addEventListener('resize', resizeCanvas, false);
     document.addEventListener('keydown', function (event) {
         handleKey(event.code);
-
     });
     function resizeCanvas(canvas) {
         initCanvas(canvas);
 
         ctx = mainCanvas.getContext("2d");
-        snake.startGame();
-        snake2.startGame();
+        if(numberOfSnakes >= 1)  snake1.startGame();
+        if(numberOfSnakes >= 2) snake2.startGame();
+        if(numberOfSnakes >= 3) snake3.startGame();
         mainLoop(ctx);
     }
     resizeCanvas(mainCanvas);
 }
 
-main();
-
-
-
-
-
-// TODO:
-//var start = null;
-//var element = document.getElementById("main");
-////element.style.position = 'absolute';
-//
-//function step(timestamp) {
-//    if (!start)
-//        start = timestamp;
-//    var progress = timestamp - start;
-//    element.style.left = Math.min(progress / 10, 200) + "px";
-//    if (progress < 2000) {
-//        window.requestAnimationFrame(step);
-//    }
-//}
-//
-//window.requestAnimationFrame(step);
+bootbox.prompt({
+    title: "<h1>Play a litte round of Thrake2d </h1>\n\
+            <i>by ittner.it</i><br />\n\
+            <img class=\"img img-responsive\" src=\"img/screen.png\"/> <br /><br />\n\
+            <p>Select the number of players (1-3)</p>",
+    inputType: 'select',
+    inputOptions: [
+        {
+            text: '1 Player',
+            value: '1',
+        },
+        {
+            text: '2 Players',
+            value: '2',
+        },
+        {
+            text: '3 Players',
+            value: '3',
+        }
+    ],
+    callback: function (result) {
+        if(result != 1 && result != 2 && result != 3) {
+            main(1);
+        } else {
+            main(result);
+        }
+    }
+});
--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -10,7 +10,10 @@ function createSnake(snakename, snakecolor) {
                 "movementX": 1,
                 "movementY": 0,
                 "score": 0,
+                "sound_hit": new Audio("audio/hit.wav"),
+                "sound_collission": new Audio("audio/collission.wav"),
                 "otherSnakes": [],
+                "availableStrawberries": [],
                 "makeKnownTo": function (otherSnake) {
                     this.otherSnakes.push(otherSnake);
                 },
@@ -41,6 +44,15 @@ function createSnake(snakename, snakecolor) {
                         ctx.lineTo(lastPoint.x, lastPoint.y);
                         ctx.stroke();
                     }
+                    console.log("Len: " + this.availableStrawberries.length);
+                    // Draw Strawberries
+                    for(i=0; i<this.availableStrawberries.length;i++) {
+                        console.log("Drawing Strawberry");
+                        var strawberry = this.availableStrawberries[i]; 
+                        ctx.fillStyle = "red";
+                        ctx.strokeStyle = "red";
+                        ctx.fillRect(strawberry.x - strawberry.radius, strawberry.y - strawberry.radius, strawberry.radius, strawberry.radius);
+                    }
                 },
                 "moveStep": function () {
                     // Perform Movement
@@ -56,7 +68,12 @@ function createSnake(snakename, snakecolor) {
                     console.log("game over");
                     ctx.font = "40px Arial";
                     ctx.fillText("X", this.currentX - 10, this.currentY + 22);
+                    if(this.speed !== 0) {
+                        // Just play the sound the first time of the function call
+                        this.sound_collission.play();
+                    }
                     this.speed = 0;
+                    
                 },
                 "startGame": function () {
                     this.score = 0;
@@ -64,6 +81,15 @@ function createSnake(snakename, snakecolor) {
                     var quaterHeight = Math.floor(mainCanvas.height / 4);
                     this.currentX = Math.floor((Math.random() * (quaterWidth + quaterWidth)) + 1) - 1 + quaterWidth;
                     this.currentY = Math.floor((Math.random() * (quaterHeight + quaterHeight)) + 1) - 1 + quaterHeight;
+                    
+                    // Create some strawberries
+                    this.AddNewStrawberry({
+                        "score": 50,
+                        "radius": 15,
+                        "x":400,
+                        "y":400
+                    });
+                    
                     this.handleDirection("down");
                     this.handleDirection("right");
                     this.handleDirection("down");
@@ -97,6 +123,21 @@ function createSnake(snakename, snakecolor) {
                                 this.handleDirection("up");
                                 break;
                             case "KeyS":
+                                this.handleDirection("down");
+                                break;
+                        }
+                    } else if (this.keyConfiguration === "ijkl") {
+                        switch (key) {
+                            case "KeyJ":
+                                this.handleDirection("left");
+                                break;
+                            case "KeyL":
+                                this.handleDirection("right");
+                                break;
+                            case "KeyI":
+                                this.handleDirection("up");
+                                break;
+                            case "KeyK":
                                 this.handleDirection("down");
                                 break;
                         }
@@ -138,10 +179,71 @@ function createSnake(snakename, snakecolor) {
                         this.corners.push({"x": this.currentX, "y": this.currentY});
                     }
                 },
+                "checkStrawberryCollission": function(pos) {
+                    var hitsStrawberry = false;
+                    var strawberryRes = null;
+                    for(i=0; i<this.availableStrawberries.length; i++) {
+                        var strawberry = this.availableStrawberries[i];
+                        if((strawberry.x - strawberry.radius) <= pos.x && (strawberry.x + strawberry.radius) >= pos.x &&
+                           (strawberry.y - strawberry.radius) <= pos.y && (strawberry.y + strawberry.radius) >= pos.y) {
+                            hitsStrawberry = true;
+                            strawberryRes = strawberry;
+                            break;
+                        }
+                    }
+                    return {
+                        "isHit":hitsStrawberry,
+                        "strawberry":strawberryRes 
+                    };
+                },
+                "RemoveAvailableStrawberry": function(strawberry) {
+                    var index = this.availableStrawberries.indexOf(strawberry);
+                    if (index > -1) {
+                        this.availableStrawberries = this.availableStrawberries.splice(index, 1);
+                    }
+                    this.availableStrawberries.pop();
+                },
+                "AddNewStrawberry": function(strawberry) {
+                    this.availableStrawberries.push(strawberry);
+                },
                 "executeGameStep": function (ctx) {
                     var myPossiblePosition = {"x": this.currentX + this.movementX, "y": this.currentY + this.movementY};
-                    var enemy = this.otherSnakes[0];
-                    if (this.isMovementPossible(myPossiblePosition) && enemy.isMovementPossible(myPossiblePosition)) {
+                    var isMovementPossible = this.isMovementPossible(myPossiblePosition);
+                    if(isMovementPossible) { // if movement is not possible here, we don't have to check the other snakes...
+                        for (index = 0; index < this.otherSnakes.length; index++) {
+                            var enemy = this.otherSnakes[index];
+                            if(enemy.isMovementPossible(myPossiblePosition) === false) {
+                                isMovementPossible = false;
+                                break;
+                            }
+                        }
+                    }
+                    if(isMovementPossible) {                        
+                        // Check if we are on a strawberry...
+                        var strawberryCheck = this.checkStrawberryCollission(myPossiblePosition);
+                        if(strawberryCheck.isHit === true) {
+                            this.sound_hit.play();
+                            // prepare a new strawberry for myself and others
+                            var quaterWidth = Math.floor(mainCanvas.width / 4);
+                            var quaterHeight = Math.floor(mainCanvas.height / 4);
+
+                            // Create a new strawberry
+                            var newStrawberry = {
+                                "score": 50,
+                                "radius": 15,
+                                "x":Math.floor((Math.random() * (quaterWidth + quaterWidth)) + 1) - 1 + quaterWidth,
+                                "y":Math.floor((Math.random() * (quaterHeight + quaterHeight)) + 1) - 1 + quaterHeight
+                            };
+                            this.score += strawberryCheck.strawberry.score;
+                            this.availableStrawberries.pop();
+                            // Inform others of this new strawberry...
+                            for(j=0; j<this.otherSnakes.length; j++) {
+                                var other = this.otherSnakes[j];
+                                other.RemoveAvailableStrawberry(strawberryCheck.strawberry);
+                                other.AddNewStrawberry(newStrawberry);
+                            }
+                            this.AddNewStrawberry(newStrawberry);
+                        }
                         this.moveStep();
                     } else {
                         this.endGame(ctx);
